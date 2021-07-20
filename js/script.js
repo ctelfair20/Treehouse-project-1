3451168c1c61216dd37ceb4b06e7f4b3997a05e6
ctelfair20 <ctelfair20@gmail.com>
2021-07-20 14:16:38 -0500

    created printQuote function
    further testing of func in browser needs to be done,
    edited quotes arr to have a source property instead of an author property

--- js/script.js
+++ js/script.js
@@ -11,14 +11,13 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 const quotes = [
-  {quote: 'Life is what happens when you\'re busy making other plans', author: 'John Lennon'},
-  {quote: 'If you look at what you have in life, you\'ll always have more. If you look at what you don\'t have in life, you\'ll never have enough.', author: 'Oprah Winfrey'},
-  {quote: 'You will face many defeats in life, but never let yourself be defeated.', author: 'Maya Angelou'},
-  {quote: 'Never let the fear of striking out keep you from playing the game.', author: 'Babe Ruth'},
-  {quote: 'I have decided to stick with love. . . . Hate is too great a burden to bear.', author: 'Martin Luther King Jr', citation: "Where Do We Go From Here?", year: '1967'}
+  {quote: 'Life is what happens when you\'re busy making other plans', source: 'John Lennon'},
+  {quote: 'If you look at what you have in life, you\'ll always have more. If you look at what you don\'t have in life, you\'ll never have enough.', source: 'Oprah Winfrey'},
+  {quote: 'You will face many defeats in life, but never let yourself be defeated.', source: 'Maya Angelou'},
+  {quote: 'Never let the fear of striking out keep you from playing the game.', source: 'Babe Ruth'},
+  {quote: 'I have decided to stick with love. . . . Hate is too great a burden to bear.', source: 'Martin Luther King Jr', citation: "Where Do We Go From Here?", year: '1967'}
 ];
 
-console.log(quotes);
 
 /***
  * `getRandomQuote` function
@@ -36,11 +35,32 @@ console.log(getRandomQuote(quotes));
  * `printQuote` function
 ***/
 
+let htmlString = ''
 
+function printQuote (quoteArr) {
+  let randomQuoteObj = getRandomQuote(quoteArr);
+  console.log(randomQuoteObj);
+  htmlString = `<p class="quote">${randomQuoteObj.quote}</p>
+  <pclass="source">${randomQuoteObj.source}`
 
+  if (randomQuoteObj.citation !== undefined ) {
+    htmlString += `<span class="citation">${randomQuoteObj.citation}</span>`
+  }
+
+  if (randomQuoteObj.year !== undefined ) {
+    htmlString += `<span class="year">${randomQuoteObj.year}</span>`
+  }
+
+  htmlString += '</p>';
+  console.log(htmlString);
+}
+
+console.log(printQuote(quotes));
+
+document.getElementById('quote-box').innerHTML = htmlString; 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-// document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
