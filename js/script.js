def09038ee9c5bfab98939e2dc7ccfc5091a29d8
ctelfair20 <ctelfair20@gmail.com>
2021-07-20 00:44:11 -0500

    added quotes array

--- js/script.js
+++ js/script.js
@@ -10,7 +10,13 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [
+  {quote: 'Life is what happens when you\'re busy making other plans', author: 'John Lennon'},
+  {quote: 'If you look at what you have in life, you\'ll always have more. If you look at what you don\'t have in life, you\'ll never have enough.', author: 'Oprah Winfrey'},
+  {quote: 'You will face many defeats in life, but never let yourself be defeated.', author: 'Maya Angelou'},
+  {quote: 'Never let the fear of striking out keep you from playing the game.', author: 'Babe Ruth'},
+  {quote: 'I have decided to stick with love. . . . Hate is too great a burden to bear.', author: 'Martin Luther King Jr'},
+]
 
 
 /***
